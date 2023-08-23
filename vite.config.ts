61584c2748d37411e47f27e249b61672204b112d
shannonwells <shannonwells@users.noreply.github.com>
2023-08-22 17:14:01 -0700

    fix testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import { resolve } from 'path'
 
 export default defineConfig({
@@ -10,5 +10,8 @@ export default defineConfig({
 			$components: resolve(__dirname, 'src/components'),
 			$routes: resolve(__dirname, 'src/routes'),
 		},
+	},
+	test: {
+		include: ['src/**/*.test.ts']
 	}
 });
