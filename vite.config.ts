3d97b008ef96e8ad2f4a057e762bdd620a717f68
shannonwells <shannonwells@users.noreply.github.com>
2023-08-22 17:04:26 -0700

    bring over config and other stuff from PD

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import { resolve } from 'path'
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+	resolve: {
+		alias: {
+			$lib: resolve(__dirname, 'src/lib'),
+			$components: resolve(__dirname, 'src/components'),
+			$routes: resolve(__dirname, 'src/routes'),
+		},
 	}
 });
