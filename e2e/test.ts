65f1a7aa834ce20195a72551b7921a99fb9cccad
shannonwells <shannonwells@users.noreply.github.com>
2023-08-31 14:12:47 -0700

    remove some unnecessary cnosts

--- e2e/test.ts
+++ e2e/test.ts
@@ -1,7 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-const colonSlashSlash = '%3A%2F%2F';
-const rococo = `wss${colonSlashSlash}rpc.rococo.frequency.xyz`;
+const rococo = `wss$%3A%2F%2Frpc.rococo.frequency.xyz`;
 test('sign-in page has expected h1', async ({ page }) => {
   await page.goto('/signin');
   await expect(page.getByRole('heading', { name: 'Sign in' })).toBeVisible();
